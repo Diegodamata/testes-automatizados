07a02ba53c6d863f3e7b2d148d36ccc0421a198d
Diego <Diegodamata>
2026-04-08 20:21:10 -0300

    Utilizando o pacote assertj para mais opcoes de assertivas

--- src/test/java/com/github/diegodamata/locadora/model/ClienteTest.java
+++ src/test/java/com/github/diegodamata/locadora/model/ClienteTest.java
@@ -1,5 +1,6 @@
 package com.github.diegodamata.locadora.model;
 
+import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,6 +18,22 @@ class ClienteTest {
         Assertions.assertNotNull(nome);
         Assertions.assertTrue(nome.startsWith("M"));
         Assertions.assertFalse(nome.length() == 100);
+
+        //assertions do pacote assertj
+        assertThat(nome).isEqualTo("Maria");
+        assertThat(nome).isLessThan("MariaS");
+        assertThat(nome.length()).isLessThan(50);
+        assertThat(nome).contains("r");
     }
 
+    @Test
+    @DisplayName("Deve criar cliente sem nome")
+    void deveCriarClienteSemNome(){
+
+        Cliente cliente = new Cliente(null);
+
+        String nome = cliente.getNome();
+
+        Assertions.assertNull(nome);
+    }
 }
