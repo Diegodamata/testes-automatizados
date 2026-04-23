ddcc8bffba8f333ce4cb63b67b057f1e8a5b0861
Diego <Diegodamata>
2026-04-23 20:03:45 -0300

    Atualizando carro na camada service

--- src/main/java/com/github/diegodamata/locadora/service/CarroService.java
+++ src/main/java/com/github/diegodamata/locadora/service/CarroService.java
@@ -1,6 +1,7 @@
 package com.github.diegodamata.locadora.service;
 
 import com.github.diegodamata.locadora.entity.Carro;
+import com.github.diegodamata.locadora.exeptions.EntityNotFoundException;
 import com.github.diegodamata.locadora.repository.CarroRepository;
 import org.springframework.stereotype.Service;
 
@@ -18,5 +19,20 @@ public class CarroService {
             throw new IllegalArgumentException("O valor da diária não pode ser negativo ou zero!");
         }
         return repository.save(carro);
+    }
+
+    public Carro atualizar(Long id, Carro carro){
+        var carroEncontrado = repository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Carro não encontrado!"));
+
+        atulizarCarroEncontrado(carro, carroEncontrado);
+
+        return repository.save(carroEncontrado);
+    }
+
+    private static void atulizarCarroEncontrado(Carro carro, Carro carroEncontrado) {
+        carroEncontrado.setAno(carro.getAno());
+        carroEncontrado.setModelo(carro.getModelo());
+        carroEncontrado.setValorDiaria(carro.getValorDiaria());
     }
 }
