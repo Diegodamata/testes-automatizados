8bc27a0245c4170814b9f7df1c73559691a4988d
Diego <Diegodamata>
2026-04-28 19:21:25 -0300

    Simulando uma busca de um carro com mockito

--- src/test/java/com/github/diegodamata/locadora/service/CarroServiceTest.java
+++ src/test/java/com/github/diegodamata/locadora/service/CarroServiceTest.java
@@ -1,11 +1,15 @@
 package com.github.diegodamata.locadora.service;
 
+import com.github.diegodamata.locadora.entity.Carro;
 import com.github.diegodamata.locadora.repository.CarroRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,5 +28,13 @@ class CarroServiceTest {
     @Test
     void deveSalvarUmCarro(){
 
+        // mock ele é vazio, se chamar algum valor ele retornara o valor padrao
+        // para criar testes eu preciso forcar uma possivel busca de dados
+
+        Mockito.when(carroRepository.findById(1L))
+                .thenReturn(Optional.of(new Carro("Honda HRV", 150.0, 2015)));
+
+        Optional<Carro> carroEncontrado = carroRepository.findById(1L);
+        System.out.println(carroEncontrado.get().getModelo());
     }
 }
